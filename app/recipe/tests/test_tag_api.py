730b27623fca94d5ebec6104b679429a30d22e21
Mike <mikerockmusic51@gmail.com>
2025-01-28 08:02:26 +0100

    feat: add update functionality for tags; implement test for updating a tag

--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -15,6 +15,11 @@ from core.tests.test_models import create_user
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def get_detail_url(tag_id):
+    """Constructs and returns the tag detail url"""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_tag(**params):
     """Creates and returns a new tag"""
     return Tag.objects.create(**params)
@@ -62,3 +67,17 @@ class PrivateTagTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Tests updating a tag"""
+        tag = Tag.objects.create(user=self.user, name="Delicious")
+        tag_url = get_detail_url(tag.id)
+
+        payload = {'name': 'Delicioso'}
+
+        res = self.client.patch(tag_url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
