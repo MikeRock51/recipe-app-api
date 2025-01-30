5a7c6b1ee88eba27382994182fe603accdee22f8
Mike <mikerockmusic51@gmail.com>
2025-01-30 09:01:06 +0100

    feat: add Ingredient model and associate with Recipe

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -79,3 +79,10 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=user, name='Test Tag')
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating a new ingredient"""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(user=user, name='Salt')
+
+        self.assertEqual(str(ingredient), ingredient.name)
