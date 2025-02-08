633d97ac70efa808ecdfebad6891ca6ad1b476f8
Mike <mikerockmusic51@gmail.com>
2025-02-08 07:19:16 +0100

    feat: handle creating recipe with ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -24,25 +24,36 @@ class IngredientSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """Recipe serializer"""
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ["id", "title", "time_minutes", "price", "link", "tags"]
+        fields = ["id", "title", "time_minutes", "price", "link", "tags", "ingredients"]
         read_only_fields = ["id"]
 
     def _get_or_create_tags(self, tags, recipe):
-        """Gets or create a tags"""
+        """Gets or create recipe tags"""
         auth_user = self.context['request'].user
 
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingreidients, recipe):
+        """Handles get or create recipe ingredients"""
+        auth_user = self.context ['request'].user
+
+        for ingredient in ingreidients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(user=auth_user, **ingredient)
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """Custom create recipe method"""
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
