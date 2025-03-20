b0fcd24cb7a6931e3f398fe22f06a6498fdd4f78
Mike <mikerockmusic51@gmail.com>
2025-03-20 09:34:10 +0100

    feat: update environment variables and improve Django settings for deployment

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#=3s!ny#^$0n!b-19y2g)aira=i1cffw--=ec5kjg7xqz!+$5e"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(None, os.environ.get('ALLOWED_HOSTS', '').split(',')),
+)
 
 
 # Application definition
